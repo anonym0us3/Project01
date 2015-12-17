01289ebfa309c4f73e78290258a7711ac02aad48
anonym0us3 <nathan.shayevich@gmail.com>
2015-12-17 11:42:27 -0800

    getting ready to push to heroku

--- models/index.js
+++ models/index.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/cartopiary");
+mongoose.connect( process.env.MONGOLAB_URI ||
+	              process.env.MONGOHQ_URL || 
+	              "mongodb://localhost/cartopiary");
 
 //Prospects db
 var Prospect = require('./prospect');
